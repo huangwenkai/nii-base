9f2c422807cb49b28f6b7e60742f91c8b1e02ddc
ker <m13128755440@163.com>
2024-07-22 10:05:59 +0800

    添加打包分析插件，默认打开

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import Components from "unplugin-vue-components/vite";
 // API自动导入
 import AutoImport from "unplugin-auto-import/vite";
 
+// 打包分析
+import { visualizer } from "rollup-plugin-visualizer";
+
 export default defineConfig({
   base: "/",
   server: {
@@ -42,6 +45,10 @@ export default defineConfig({
       // resolvers: [ArcoResolver()],
       // 文件扩展
       extensions: ["vue"],
+    }),
+    // 打包之后打开分析页面
+    visualizer({
+      open: true,
     }),
   ],
   css: {
